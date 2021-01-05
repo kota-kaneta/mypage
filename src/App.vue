c1dcfecfeda56967a16c709174d1d142c6c6cbd7
kota-kaneta <foruri@icloud.com>
2021-01-06 08:01:50 +0900

    タイム

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/skillset" >Skillset</router-link> |
       <router-link to="portfolio">Portfolio</router-link> 
-      <router-link to="/contact">Contact</router-link>
+      <!-- <router-link to="/contact">Contact</router-link> -->
     </div>
     <hr id="hr">
       <router-view/>
