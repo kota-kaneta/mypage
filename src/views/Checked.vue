8004d8a5aad6d7bd75b382af1393cbf6e045ec8e
kota-kaneta <foruri@icloud.com>
2021-08-11 21:13:41 +0900

    Update Checked.vue

--- src/views/Checked.vue
+++ src/views/Checked.vue
@@ -2,14 +2,14 @@
   <div id="checked">
     <form action="cgi-bin/abc.cgi" method="post">
       <div>
-        <input type="checkbox" name="riyu" value="1" checked="checked" />マインド
-        <input type="checkbox" name="riyu" value="2" />服装
-        <input type="checkbox" name="riyu" value="3" />靴
-        <input type="checkbox" name="riyu" value="4" />顎肉
-        <input type="checkbox" name="riyu" value="5" />腹肉
-        <input type="checkbox" name="riyu" value="6" />トーク力
-        <input type="checkbox" name="riyu" value="7" />髪型
-        <input type="checkbox" name="riyu" value="8" />匂い
+        <input type="checkbox" name="riyu" value="1" checked="" />マインド
+        <input type="checkbox" name="riyu" value="2" checked="" />服装
+        <input type="checkbox" name="riyu" value="3" checked="" />靴
+        <input type="checkbox" name="riyu" value="4" checked="" />顎肉
+        <input type="checkbox" name="riyu" value="5" checked="" />腹肉
+        <input type="checkbox" name="riyu" value="6" checked="" />トーク力
+        <input type="checkbox" name="riyu" value="7" checked="" />髪型
+        <input type="checkbox" name="riyu" value="8" checked="" />匂い
       </div>
       <p>
         <input type="submit" value="送信する" />
